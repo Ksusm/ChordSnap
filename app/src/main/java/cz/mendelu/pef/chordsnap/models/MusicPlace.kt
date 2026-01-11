1ed1881b53dd5454ce34ac78ef30de2f03b59adc
Kseniia Kuznetsova <xkuznet4@mendelu.cz>
2026-01-11 02:03:30 +0100

    Icons to be used in a map according to the type of music place, added a picture to the home screen

--- app/src/main/java/cz/mendelu/pef/chordsnap/models/MusicPlace.kt
+++ app/src/main/java/cz/mendelu/pef/chordsnap/models/MusicPlace.kt
@@ -2,6 +2,7 @@ package cz.mendelu.pef.chordsnap.models
 
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.serialization.Serializable
+import cz.mendelu.pef.chordsnap.R
 
 @Serializable
 data class MusicPlace(
@@ -25,11 +26,14 @@ data class MusicPlacesResponse(
     val places: List<MusicPlace>
 )
 
-enum class MusicPlaceType {
-    MUSIC_SHOP,
-    MUSIC_SCHOOL,
-    CONSERVATORY,
-    MUSIC_ACADEMY,
-    RECORDING_STUDIO,
-    REHEARSAL_ROOM
+enum class MusicPlaceType(
+    val label: String,
+    val iconRes: Int
+) {
+    MUSIC_SHOP("Music Shop", R.drawable.shop),
+    MUSIC_SCHOOL("Music School", R.drawable.school),
+    CONSERVATORY("Conservatory", R.drawable.academy),
+    MUSIC_ACADEMY("Music Academy", R.drawable.academy),
+    RECORDING_STUDIO("Recording Studio", R.drawable.record),
+    REHEARSAL_ROOM("Rehearsal Room", R.drawable.rehearsal)
 }
